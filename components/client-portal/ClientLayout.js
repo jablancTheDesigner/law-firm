44862738a0cc4388740e803bf89c1039d0b56707
Jahmal Blanchard <jablancdesigns@gmail.com>
2025-03-12 09:15:18 -0500

    Unified nav component

--- components/client-portal/ClientLayout.js
+++ components/client-portal/ClientLayout.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Nav from "../Nav";
 import { useAppContext } from "../../context/appContext";
 
 const ClientPortalLayout = ({ children }) => {
   const {brandName} = useAppContext();
   return (
     <div>
-      <header>
-        <Nav />
-      </header>
       <div className="container" style={{textAlign: "center"}}>
         <h2>{brandName} - Client Portal</h2>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.scss";
 import {AppContextProvider} from "../context/appContext";
+import Nav from "../components/Nav";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AppContextProvider>
+      <header>
+        <Nav />
+      </header>
       <Component {...pageProps} />
     </AppContextProvider>
   );
